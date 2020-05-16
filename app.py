e28509011b1274c62abe8dcbd53155dad0378486
Rhenan Bartels <rhenan.bartels@gmail.com>
2020-05-16 14:31:10 -0300

    Treat errors and set api version

--- app.py
+++ app.py
@@ -7,18 +7,33 @@ import face_recognition
 from aiohttp_cache import cache, setup_cache
 
 
-def detect_faces(img_url):
-    resp = requests.get(img_url)
-    img = face_recognition.load_image_file(BytesIO(resp.content))
+API_VERSION = "v0.1"
+
+
+def detect_faces(img_content):
+    img = face_recognition.load_image_file(BytesIO(img_content))
     return len(face_recognition.face_locations(img)) > 0
 
 
+def request_image(img_url):
+    resp = requests.get(img_url)
+    return  resp.content
+
+
 @cache()
-async def has_faces_view(request):
+async def faces_view(request):
     img_url = request.rel_url.query.get("img_url")
     if img_url:
-        response = {"has_face": detect_faces(img_url)}
-        status = 200
+        try:
+            img = request_image(img_url)
+            response = {"has_face": detect_faces(img)}
+            status = 200
+        except Exception as e:
+            response = {
+                "error": {
+                    "detail": f"not able to retrieve image from {img_url}"}
+            }
+            status = 503
     else:
         response = {"error": "img url not provided"}
         status = 400
@@ -28,4 +43,4 @@ async def has_faces_view(request):
 
 app = web.Application()
 setup_cache(app)
-app.router.add_get("/has-faces", has_faces_view)
+app.router.add_get(f"/{API_VERSION}/faces", faces_view)
